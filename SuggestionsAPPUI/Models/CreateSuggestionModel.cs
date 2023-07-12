ef290691e3f7aaa6cad4fef8bb43f2bdc5047484
BIGJOE8080 <joeoluwafemi80@gmail.com>
2023-07-12 15:29:25 +0100

    'progress'

--- SuggestionsAPPUI/Models/CreateSuggestionModel.cs
+++ SuggestionsAPPUI/Models/CreateSuggestionModel.cs
@@ -1,6 +1,20 @@
-﻿namespace SuggestionsAPPUI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionsAPPUI.Models
 {
     public class CreateSuggestionModel
     {
+        [Required]
+        [MaxLength(length: 75)]
+        public string suggestion { get; set; }
+        [Required]
+        [MinLength(length: 1)]
+        [Display(Name = "Category")]   
+
+        public string CategoryId { get; set; }
+
+        [MaxLength(length: 500)]
+        public string Description{ get; set; }
+
     }
 }
